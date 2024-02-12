bb8b3da166a6ee76106dea45b8f8cb1aebb284e6
Olga <olginarom@gmail.com>
2024-02-12 14:29:05 +0100

    <correzioni task 12>

--- src/test-12/index.ts
+++ src/test-12/index.ts
@@ -1,1 +1,3 @@
-export const sortByLength = () => {};
+type Order = "asc" | "desc";
+export const sortByLength = (names: string[], order: Order = "asc"): string[] =>
+  names.sort((a, b) => (order === "asc" ? a.length - b.length : b.length- a.length));
