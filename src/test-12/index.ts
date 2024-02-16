3672df52b1921fc13839811e4f00216b94221b7b
Olga <olginarom@gmail.com>
2024-02-16 14:28:51 +0100

    correzioni test 16

--- src/test-12/index.ts
+++ src/test-12/index.ts
@@ -1,5 +1,14 @@
-type Order = "asc" | "desc";
-export const sortByLength = (names: string[], order: Order = "asc"): string[] =>
-  names.sort((a, b) =>
-    order === "desc" ? b.length - a.length : a.length - b.length
-  );
+// export const sortByLength = (names: string[], order:'asc' | 'desc'= 'asc'): string[] =>
+//   names.sort((a, b) => order === 'desc' ? b.length - a.length : a.length - b.length);
+
+export const sortByLength = (
+  names: string[],
+  order: "asc" | "desc" = "asc"
+): string[] => {
+  return names.sort((a, b) => {
+    if (order === "asc") {
+      return a.length - b.length;
+    }
+    return b.length - a.length;
+  });
+};
