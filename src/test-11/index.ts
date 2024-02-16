1d7249280058d972e672b3f5a013552f2e7e2eb6
Olga <olginarom@gmail.com>
2024-02-16 13:35:26 +0100

    correzioni 16.02

--- src/test-11/index.ts
+++ src/test-11/index.ts
@@ -1,5 +1,5 @@
 type Order = "asc" | "desc";
 export const sortNames = (names: string[], order: Order = "asc"): string[] =>
   names.sort((a, b) =>
-    order === "asc" ? a.localeCompare(b) : b.localeCompare(a)
+    order === "desc" ? b.localeCompare(a) : a.localeCompare(b)
   );
--- src/test-13/index.ts
+++ src/test-13/index.ts
@@ -1,1 +1,7 @@
-export const sortByLength = () => {};
+type Order = "asc" | "desc";
+export const sortByLength = (names: string[], order: Order = "asc"): string[] =>
+  names.sort((a, b) =>
+    order === "desc"
+      ? b.length - a.length || b.localeCompare(a)
+      : a.length - b.length || a.localeCompare(b)
+  );
