27dabc2c4a9a796888d070fc3c62d523e40a53aa
Olga <olginarom@gmail.com>
2024-02-19 13:27:26 +0100

    correzioni test

--- src/test-11/index.ts
+++ src/test-11/index.ts
@@ -1,5 +1,6 @@
 type Order = "asc" | "desc";
-export const sortNames = (names: string[], order: Order = "asc"): string[] =>
+export const sortNames = (names: string[], order: Order = "asc"): string[] => 
   names.sort((a, b) =>
-    order === "asc" ? a.localeCompare(b) : b.localeCompare(a)
+    order === "desc" ? b.localeCompare(a) : a.localeCompare(b)
   );
+
