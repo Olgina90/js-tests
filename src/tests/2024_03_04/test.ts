80566bca2f9c2a2dfc398421932529306e0d63eb
Olga <olginarom@gmail.com>
2024-03-08 13:29:08 +0100

    exercising

--- src/tests/2024_03_04/test.ts
+++ src/tests/2024_03_04/test.ts
@@ -1,40 +1,41 @@
 //Test #1
 export const getFirstArgument = (value: unknown): unknown => {
-    return value
-}
+  return value;
+};
 
 //Test #2
-export const sum = (a:number, b: number): number => {
-    return a + b
-}
+export const sum = (a: number, b: number): number => {
+  return a + b;
+};
 
 //Test #3
-export const calc = (a:number, operator: '+' | '-' | '*' | '/', b:number) => {
-    if(operator === '+'){
-        return a + b 
-    }
-    if(operator === '-'){
-        return a - b 
-    }
-    if(operator === '*'){
-        return a * b 
-    }
-    if(operator === '/'){
-        return a / b 
-    }
-}
+export const calc = (
+  a: number,
+  operator: "+" | "-" | "*" | "/",
+  b: number
+): number => {
+  if (operator === "+") {
+    return a + b;
+  }
+  if (operator === "-") {
+    return a - b;
+  }
+  if (operator === "*") {
+    return a * b;
+  }
+  return a / b;
+};
 
 //Test #4
-export const getValueOrOne = (value?:number): number => 
-{
-    if (value !== 0){
-        return value
-    }
-    if (value === 0){
-        return 1
-    }
-}
+export const getValueOrOne = (value?: number): number => {
+  if (value !== 0) {
+    return value;
+  }
+  if (value === 0) {
+    return 1;
+  }
+};
 
 //Test #5
 
-export const max = (...values:number[]): number => Math.max(...values)
+export const max = (...values: number[]): number => Math.max(...values);
