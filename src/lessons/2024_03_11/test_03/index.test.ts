402075154ab2273de6825bf33af52634085a896e
Olga <olginarom@gmail.com>
2024-03-11 14:06:34 +0100

    classwork-11-03

--- src/lessons/2024_03_11/test_03/index.test.ts
+++ src/lessons/2024_03_11/test_03/index.test.ts
@@ -3,12 +3,12 @@ import { split } from ".";
 
 describe("quando `word` e' 'olga'", () => {
   test("`split` dovrebbe tornare la lista di lettere", () => {
-    expect(split("olga", 0)).toStrictEqual(["o", "l", "g", "a"]);
+    expect(split("olga")).toStrictEqual(["o", "l", "g", "a"]);
   });
 });
 
 describe("quando `word` e' 'davide'", () => {
   test("`split` dovrebbe tornare la lista di lettere", () => {
-    expect(split("davide", 0)).toStrictEqual(["d", "a", "v", "i", "d", "e"]);
+    expect(split("davide")).toStrictEqual(["d", "a", "v", "i", "d", "e"]);
   });
 });
