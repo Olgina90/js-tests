91fb72d4bfc0952ce90631b57777ce90be1d7aab
Olga <olginarom@gmail.com>
2024-04-05 14:35:07 +0200

    classwork

--- src/lessons/2024_04_05/test_02/index.ts
+++ src/lessons/2024_04_05/test_02/index.ts
@@ -1,1 +1,8 @@
-export const onlyEvenItems = () => {};
+const isEven = (value: number): boolean => value % 2 === 0;
+const isOdd = (value: number): boolean => !isEven(value)
+
+export const onlyEvenItems = (list: string[]): string[] =>
+  list.filter((_letter, index) => {
+    const position = index + 1;
+    return !isOdd(position);
+  });
