5eff3875712b384ad4cc0dc29c36477e1a797f9f
Davide <davidebruno1989@gmail.com>
2024-04-19 07:38:04 +0200

    lessons 2024_04_19

--- src/lessons/2024_04_19/test_01/index.test.ts
+++ src/lessons/2024_04_19/test_01/index.test.ts
@@ -3,12 +3,15 @@ import { lettersAndIndexes } from ".";
 
 describe("lettersAndIndexes", () => {
   test("Deve ritornare la lista con oggetti che hanno letter e index", () => {
-    expect(lettersAndIndexes(["A", "B", "C", "D", "E"])).toStrictEqual([
+    const input = ["A", "B", "C", "D", "E"];
+    const output = [
       { letter: "A", index: 0 },
       { letter: "B", index: 1 },
       { letter: "C", index: 2 },
       { letter: "D", index: 3 },
       { letter: "E", index: 4 },
-    ]);
+    ];
+
+    expect(lettersAndIndexes(input)).toStrictEqual(output);
   });
 });
--- src/lessons/2024_04_19/test_02/index.test.ts
+++ src/lessons/2024_04_19/test_02/index.test.ts
@@ -3,16 +3,16 @@ import { dataAndIndexes } from ".";
 
 describe("dataAndIndexes", () => {
   test("Deve ritornare la lista con oggetti che hanno gli stessi dati e index", () => {
-    expect(
-      dataAndIndexes([
-        { name: "Olga", age: 24 },
-        { surname: "Bruno", height: 1.64 },
-        { middlename: "Fernandez", weight: 75 },
-      ])
-    ).toStrictEqual([
+    const input = [
+      { name: "Olga", age: 24 },
+      { surname: "Bruno", height: 1.64 },
+      { middlename: "Fernandez", weight: 75 },
+    ];
+    const output = [
       { name: "Olga", age: 24, index: 0 },
       { surname: "Bruno", height: 1.64, index: 1 },
       { middlename: "Fernandez", weight: 75, index: 2 },
-    ]);
+    ];
+    expect(dataAndIndexes(input)).toStrictEqual(output);
   });
 });
