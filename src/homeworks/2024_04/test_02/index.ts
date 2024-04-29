818d5d5f9184c26f84c792b04d0394401902e5d7
Olga <olginarom@gmail.com>
2024-04-29 07:27:41 +0200

    homework_26_04_completed

--- src/homeworks/2024_04/test_02/index.ts
+++ src/homeworks/2024_04/test_02/index.ts
@@ -1,1 +1,7 @@
-export const reverseWords = () => {};
+const getName = (name: string): string => {
+    return name.split('').reverse().join('')
+}
+
+export const reverseWords = (list:string[]): string[] => {
+    return list.map(getName)
+};
