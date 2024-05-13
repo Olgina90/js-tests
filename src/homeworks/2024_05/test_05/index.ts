f3f635fff7e009b704e3e55059a57a40c9ba52fd
Olga <olginarom@gmail.com>
2024-05-13 07:35:20 +0200

    homework_10_05_completed

--- src/homeworks/2024_05/test_05/index.ts
+++ src/homeworks/2024_05/test_05/index.ts
@@ -1,1 +1,10 @@
-export const flatAndSortAges = () => {};
+const getAgesList = (ages: string): number[] => {
+  const listOfString = ages.split(", ");
+  const listofNumbers = listOfString.map(Number);
+  return listofNumbers;
+};
+export const flatAndSortAges = (list: string[]): number[] => {
+  const listOfAges = list.flatMap(getAgesList);
+  const orderedlistAges = listOfAges.sort((a, b) => a - b);
+  return orderedlistAges;
+};
